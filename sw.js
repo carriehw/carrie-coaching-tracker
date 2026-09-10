bc3a3cda31b0e36893ad2716d61372ed8ac376f2
carriehw <huiwun2@gmail.com>
2026-09-11 02:25:32 +0800

    Refresh cache for mobile UI polish v13

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const C='coaching-v14';
-const A=['./','./index.html','./manifest.webmanifest','./icon.svg','./coaching-v10.css?v=11','./coaching-v10.js?v=12'];
+const C='coaching-v15';
+const A=['./','./index.html','./manifest.webmanifest','./icon.svg','./coaching-v10.css?v=11','./polish-v13.css?v=13','./coaching-v10.js?v=12'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(A)))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==C).map(k=>caches.delete(k)))),self.clients.claim()])));
 self.addEventListener('fetch',e=>{
