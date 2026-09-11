87c0112ee2264b25e6abe88cc35004f7f8846c4f
carriehw <huiwun2@gmail.com>
2026-09-11 12:43:04 +0800

    Refresh cache for final v22 release

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const C='coaching-v21';
-const A=['./','./index.html','./manifest.webmanifest','./icon.svg','./coaching-v10.css?v=14','./polish-v13.css?v=13','./coaching-v15.css?v=1','./form-controls-v19.css?v=1','./appointment-v20.css?v=1','./coaching-v10.js?v=14','./coaching-v15-addon.js?v=2','./appointment-v20.js?v=1','./scroll-reset-v21.js?v=1'];
+const C='coaching-v22';
+const A=['./','./index.html','./manifest.webmanifest','./icon.svg','./coaching-v10.css?v=14','./polish-v13.css?v=13','./coaching-v15.css?v=1','./form-controls-v19.css?v=1','./appointment-v20.css?v=1','./final-v22.css?v=1','./coaching-v10.js?v=14','./coaching-v15-addon.js?v=2','./appointment-v20.js?v=1','./scroll-reset-v21.js?v=1','./final-v22.js?v=1'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(A)))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==C).map(k=>caches.delete(k)))),self.clients.claim()])));
 self.addEventListener('fetch',e=>{
