10147327517d9cd91f5009f1901b98dc1938545a
carriehw <huiwun2@gmail.com>
2026-09-11 11:39:43 +0800

    Refresh cache after load freeze fix

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const C='coaching-v17';
-const A=['./','./index.html','./manifest.webmanifest','./icon.svg','./coaching-v10.css?v=14','./polish-v13.css?v=13','./coaching-v15.css?v=1','./coaching-v10.js?v=14','./coaching-v15-addon.js?v=1','./coaching-v15-fix.js?v=1'];
+const C='coaching-v18';
+const A=['./','./index.html','./manifest.webmanifest','./icon.svg','./coaching-v10.css?v=14','./polish-v13.css?v=13','./coaching-v15.css?v=1','./coaching-v10.js?v=14','./coaching-v15-addon.js?v=2'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(A)))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==C).map(k=>caches.delete(k)))),self.clients.claim()])));
 self.addEventListener('fetch',e=>{
